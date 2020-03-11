cf9087c05451dd5234e3ca59620c145d8023ce55
Jim <51113579+jim11662418@users.noreply.github.com>
2020-03-11 08:50:21 -0400

    Update analogClock.lua

--- analogClock.lua
+++ analogClock.lua
@@ -1,3 +1,19 @@
+-----------------------------------------------------------------------------------------------------------------------------------
+-- Copyright © 2020 Jim Loos
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+-- (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+-- publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+-- so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+-- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+-- IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-----------------------------------------------------------------------------------------------------------------------------------
+
 -- this script connects to an NTP server to sync the ESP8266 RTC. re-syncs with the NTP server every 15 minutes.
 -- pulses the analog clock's Lavet type stepping motor to keep the analog clock in sync with the ESP8266 RTC.
 -- in addition to the usual file, gpio, net, node, sntp, tmr, and wifi modules, the interger firmware also 
